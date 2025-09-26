9baed4897446d91a1c824881b47c4b6ce9dd30f0
Bogdan Baciu <vlabac@alu.edu.gva.es>
2025-09-26 19:59:44 +0200

    Creadas las primeras funciones de trata de gastos, trabajando en anyadirEtiquetas()

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,41 +1,110 @@
 let presupuesto = 0;
+let fechaHoy = new Date();
+
+let gastos = [];
+
+let idGasto = 0;
 
 function actualizarPresupuesto(a) {
-    if (a >= 0) {
-        presupuesto = a;
-        return presupuesto;
-    } else {
-        return -1;
-    }
+  if (a >= 0) {
+    presupuesto = a;
+    return presupuesto;
+  } else {
+    return -1;
+  }
 }
 
 function mostrarPresupuesto() {
-    return "Tu presupuesto actual es de " + presupuesto + " €";
+  return "Tu presupuesto actual es de " + presupuesto + " €";
 }
 
-function CrearGasto(descripcion, valor) {
-    this.descripcion = descripcion;
-    this.valor = valor >= 0 ? valor : 0;
+function CrearGasto(descripcion, valor, fecha, etiquetas) {
+  this.descripcion = descripcion;
+  this.valor = valor >= 0 ? valor : 0;
+  this.fecha = fecha >= null ? fecha : fechaHoy.toDateString;
+  this.etiquetas = etiquetas >= null ? etiquetas : [];
 
-    this.mostrarGasto = function() {
-        return "Gasto correspondiente a " + this.descripcion + " con valor " + this.valor + " €";
-    };
 
-    this.actualizarDescripcion = function(nuevaDescripcion) {
-        this.descripcion = nuevaDescripcion;
-    };
+  this.mostrarGastoCompleto = function () {
+    return (
+      "Gasto correspondiente a " +
+      this.descripcion +
+      " con valor " +
+      this.valor +
+      " €.\n" +
+      "Fecha: " + 
+      this.fecha +
+      "\nEtiquetas: "
+      + this.gastos
+    );
+  };
 
-    this.actualizarValor = function(nuevoValor) {
-        if (nuevoValor >= 0) {
-            this.valor = nuevoValor;
-        } else {
-            console.log("Error: El valor introducido no es válido.");
+  this.actualizarDescripcion = function (nuevaDescripcion) {
+    this.descripcion = nuevaDescripcion;
+  };
+
+  this.actualizarValor = function (nuevoValor) {
+    if (nuevoValor >= 0) {
+      this.valor = nuevoValor;
+    } else {
+      console.log("Error: El valor introducido no es válido.");
+    }
+  };
+
+  this.anyadirEtiquetas = function (...nuevasEtiquetas)
+  {
+    for(let i = 0; i < nuevasEtiquetas.length; i++)
+    {
+        if(this.etiquetas.includes(nuevasEtiquetas[i]) != false)
+        {
+            this.etiquetas.push(nuevasEtiquetas[i]);
         }
-    };
+    }
+  }
 }
 
-export {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto
+function listarGastos() {
+  return gastos;
 }
+
+function anyadirGasto(gasto) {
+    gasto.id = idGasto;
+    idGasto++;
+    gastos.push(gasto);
+}
+
+function borrarGasto(id) {
+    for(let i = 0; i < gastos.length; i++)
+    {
+        if(gastos[i].id == id)
+        {
+            gastos.splice(i, 1)
+        }
+        else if(gastos[i] == null)
+        {
+            return;
+        }
+    }
+}
+
+function calcularTotalGastos() {
+
+    let sumaGastos = 0;
+    for(let i = 0; i < gastos.length;i++)
+    { 
+        sumaGastos = sumaGastos + gastos[i].valor
+    }
+
+    return sumaGastos;
+
+}
+
+function calcularBalance() {
+    let balance;
+
+    balance = presupuesto - calcularTotalGastos();
+    return balance;
+
+}
+
+export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto, listarGastos, anyadirGasto, borrarGasto, calcularTotalGastos, calcularBalance };
